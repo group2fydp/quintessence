ad1f011771a1342fb1fdbd43d3c5e01786f75f21
Jon <jonathanloos@rogers.com>
2020-01-16 14:22:44 -0500

    Mapper now functioning.

--- safetyplan/src/main/java/com/cove/safetyplan/dto/mapper/SafetyPlanMapper.java
+++ safetyplan/src/main/java/com/cove/safetyplan/dto/mapper/SafetyPlanMapper.java
@@ -6,10 +6,10 @@ import com.cove.safetyplan.model.SafetyPlan;
 public class SafetyPlanMapper {
     public static SafetyPlanDto toSafetyPlanDto(SafetyPlan safetyPlan) {
         return new SafetyPlanDto()
-                .setId(safetyPlan.getId())
-                .setStudent_id(safetyPlan.getStudent_id())
-                .setClinician_id(safetyPlan.getClinician_id())
-                .setCreated_at(safetyPlan.getCreated_at())
-                .setLast_modified_at(safetyPlan.getLast_modified_at());
+                .setSafetyplanId(safetyPlan.getSafetyplanId())
+                .setStudentId(safetyPlan.getStudentId())
+                .setClinicianId(safetyPlan.getClinicianId())
+                .setCreateDate(safetyPlan.getCreateDate())
+                .setLastModifyDate(safetyPlan.getLastModifyDate());
     }
 }
